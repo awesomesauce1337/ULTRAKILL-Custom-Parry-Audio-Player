9971163ffb8dcc512c73f4834cb7cbce567d046d
awesomesauce1337 <142626333+awesomesauce1337@users.noreply.github.com>
2024-06-05 17:33:19 -0400

    Update PluginInfo.cs

--- UK-CPAP/PluginInfo.cs
+++ UK-CPAP/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace ultrakillParrySoundAndAudioReplacer
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "ultrakillParrySoundAndAudioReplacer";
-        public const string PLUGIN_NAME = "ultrakillParrySoundAndAudioReplacer";
+        public const string PLUGIN_GUID = "com.awesomesauce.UltraKill.UKCPAP";
+        public const string PLUGIN_NAME = "UKCPAP";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
